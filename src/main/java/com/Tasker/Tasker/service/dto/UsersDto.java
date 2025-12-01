9d0d37334d1c7af6f519909566ab0d44b5f26c63
Márton <kolozsi.ocsi@gmail.com>
2025-12-01 14:38:29 +0100

    add UsersDto to the project

---
+++ src/main/java/com/Tasker/Tasker/service/dto/UsersDto.java
@@ -0,0 +1,56 @@
+package com.Tasker.Tasker.service.dto;
+
+import java.util.Objects;
+
+public class UsersDto {
+
+    private Long id;
+    private String name;
+    private String email;
+
+    public UsersDto() {
+    }
+
+    public UsersDto(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UsersDto that = (UsersDto) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+}
