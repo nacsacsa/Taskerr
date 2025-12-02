a59a3b90750d329adb815196d9e0a665e3888f1b
Márton <kolozsi.ocsi@gmail.com>
2025-12-02 10:52:51 +0100

    add ModelMapper to the project

--- src/main/java/com/Tasker/Tasker/TaskerApplication.java
+++ src/main/java/com/Tasker/Tasker/TaskerApplication.java
@@ -1,7 +1,9 @@
 package com.Tasker.Tasker;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskerApplication {
@@ -9,5 +11,8 @@ public class TaskerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskerApplication.class, args);
 	}
-
+	@Bean
+	ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
