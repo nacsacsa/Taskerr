1df36c4426b43b169ba49e3e9de9c4b5d743944f
Márton <kolozsi.ocsi@gmail.com>
2025-12-16 11:39:02 +0100

    add components to frontend

---
+++ frontend/src/app/tables/tables.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tables',
+  imports: [],
+  templateUrl: './tables.html',
+  styleUrl: './tables.css',
+})
+export class Tables {
+
+}
